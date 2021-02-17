15a7c795ed3f78228577f056f77b0d1bc8fb29c7
JsEnthusiast <54439226+J4Web@users.noreply.github.com>
2021-02-17 15:46:02 -0800

    Put these in Src

---
+++ HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from './Nav';
+import Banner from './Banner';
+function HomeScreen() {
+    return (
+        <div className="homescreen">
+            <Nav/>
+            <Banner/>
+            {/* NavBAR */}
+            {/* BANNER */}
+            {/* ROWS */}
+        </div>
+    )
+}
+
+export default HomeScreen
---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDWjrp0yCLxebTq40v8NGizyWewh9quxvM",
+    authDomain: "neflix-95d46.firebaseapp.com",
+    projectId: "neflix-95d46",
+    storageBucket: "neflix-95d46.appspot.com",
+    messagingSenderId: "340897481755",
+    appId: "1:340897481755:web:61f3a239744826d93fa4be"
+};
+
